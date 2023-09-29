4841ec8dd70253528075ae4d618b150c8fedd62c
AveryVi <stepan.myrsko@gmail.com>
2023-09-29 20:47:17 +0200

    added the food class added spawnFood function

--- datastructurePrototype.cpp
+++ datastructurePrototype.cpp
@@ -14,35 +14,49 @@ class playfield{
 };
 class player{
     public:
-    char headposX;
-    char headposY;
-    char tailX[128];
-    char tailY[128];
-    char direction;
-    char length;
-    player(char pheadposX, char pheadposY, char pdirection, char plenght){
-        headposX = pheadposX;
-        headposY = pheadposY;
-        direction = pdirection;
-        length = plenght;
-    };
-    void spawn(){
-        headposX = headposX;
-        headposY = headposY;
-        direction = 0;
-        length = 3;
-    }
-    void move(){
-        for(int i = length; i >= 0; i--){
-            tailX[i] = tailX[i-1];
-            tailY[i] = tailY[i-1];
+        char headposX;
+        char headposY;
+        char tailX[128];
+        char tailY[128];
+        char direction;
+        char length;
+        player(char pheadposX, char pheadposY, char pdirection, char plenght){
+            headposX = pheadposX;
+            headposY = pheadposY;
+            direction = pdirection;
+            length = plenght;
+        };
+        void spawn(){
+            headposX = headposX;
+            headposY = headposY;
+            direction = 0;
+            length = 3;
         }
-        if (direction == 0) headposY = headposY + 1;
-        else if (direction == 1) headposX = headposX + 1;
-        else if (direction == 2) headposY = headposY - 1;
-        else if (direction == 3) headposX = headposX - 1;
-    }
+        void move(){
+            for(int i = length; i >= 0; i--){
+                tailX[i] = tailX[i-1];
+                tailY[i] = tailY[i-1];
+            }
+            if (direction == 0) headposY = headposY + 1;
+            else if (direction == 1) headposX = headposX + 1;
+            else if (direction == 2) headposY = headposY - 1;
+            else if (direction == 3) headposX = headposX - 1;
+        }
 };
+class food{
+    public:
+        char x;
+        char y;
+        char nutrition;
+        food(char x, char y, char val){
+            x = x;
+            y = y;
+            nutrition = val;
+        }
+};
+void spawnFood(){
+    food food(0,0,0);
+}
 //interaction
 void arrowControls(){
 }
