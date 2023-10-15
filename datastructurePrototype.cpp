83ad5698c4abcf0b127e15effdd30a7fbebf7c60
AveryVi <stepan.myrsko@gmail.com>
2023-10-15 23:16:49 +0200

    preformance tweks to game tick

--- datastructurePrototype.cpp
+++ datastructurePrototype.cpp
@@ -185,12 +185,13 @@ void gameTick(){/*Tested - fully working as intended*/
         playerEat(Player1);
         playerEat(Player2);
     }
-    if ((playercheck(Player1, Player2) == 0) && (playercheck(Player2, Player1) == 0)){
+    else if ((playercheck(Player1, Player2) == 0) && (playercheck(Player2, Player1) == 0)){
         cout << "colision" << endl;
     }
     if((Player1.movecheck() == 0) && (Player2.movecheck() == 0)) cout << "both walled" << endl;
     else if(Player1.movecheck() == 0) cout << "player1 walled" << endl;
     else if(Player2.movecheck() == 0) cout << "player2 walled" << endl;
+    else cout << "nothing happened" << endl;
 }          
 void testGame(){
     printf("Player1: x= %d, y= %d, direction= %d  tail: \r\nlength= %d, first6x= %d, %d, %d, %d, %d, %d, first6y= %d, %d, %d, %d, %d, %d \r\nlengthStored= %d, %d, %d, %d, %d\r\n\n", Player1.headposX, Player1.headposY, Player1.direction, Player1.length, Player1.tailX[0], Player1.tailX[1], Player1.tailX[2], Player1.tailX[3], Player1.tailX[4], Player1.tailX[5], Player1.tailY[0], Player1.tailY[1], Player1.tailY[2], Player1.tailY[3], Player1.tailY[4], Player1.tailY[5], Player1.lengthStored[0], Player1.lengthStored[1], Player1.lengthStored[2], Player1.lengthStored[3], Player1.lengthStored[4]);
