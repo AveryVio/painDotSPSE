832d6194a74e14c5b9847af7c8014dac60125576
AveryVi <stepan.myrsko@gmail.com>
2023-11-02 20:04:53 +0100

    added a basic change velocity function

--- secondgameTEst.cpp
+++ secondgameTEst.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 #define MAXX 256
 #define MAXY 128
-#define GRAVITY 4.5
-#define AIRDRAG 3.2
+#define GRAVITY 4
+#define AIRDRAG 3
 
 class position{
     public:
@@ -16,7 +16,17 @@ class player{
     position coords;
     int velocityX;
     int velocityY;
+    void changeVelocity(char action, int x, int y);
 };
+void player::changeVelocity(char action, int x, int y){
+    if(action == 0) return;
+    else if(action == 'j') {
+        velocityX = x;
+        velocityY = y;
+    }
+    else if(action == 'g') velocityY -= (GRAVITY * 0.3 * (0.5 * AIRDRAG));
+    else if(action == 'd') velocityX -= (AIRDRAG * 0.5);
+}
 player playe;
 int main(){
     cout << "This device is sponsored by the dotSPSE project" << endl;
