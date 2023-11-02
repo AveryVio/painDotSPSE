756e408c2e21af35853b1f84eff0cf5db0f02e50
AveryVi <stepan.myrsko@gmail.com>
2023-11-02 19:47:54 +0100

    createdvery basic player class with some variables

--- secondgameTEst.cpp
+++ secondgameTEst.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class position{
+    public:
+    int xpos;
+    int ypos;
+};
+class player{
+    public:
+    position coords;
+    int velocityX;
+    int velocityY;
+};
+player playe;
 int main(){
     cout << "This device is sponsored by the dotSPSE project" << endl;
     return 0;
